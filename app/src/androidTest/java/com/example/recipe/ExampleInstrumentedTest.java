af1b12d912ee18612daebfb6a1396d3f5856742a
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-12-13 21:55:15 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/recipe/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/recipe/ExampleInstrumentedTest.java
@@ -20,6 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
+        
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.recipe", appContext.getPackageName());
